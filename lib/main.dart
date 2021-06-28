691851320517a88a9a13bb4f400395a3919d9010
stoian2662 <stoian2662@gmail.com>
2021-06-28 22:13:53 +0300

    google_mobile_ads package integrated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
@@ -8,6 +9,9 @@ import 'matching/models/difficulty_view_model.dart';
 import 'matching/models/game_view_model.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
   runApp(
     MultiProvider(
       providers: [
